15888ab847a23c2cd248fba82f8f8686ea2bcaf6
Ebunwadi <nwadiokwu.ebube@gmail.com>
2023-09-14 21:13:11 +0100

    add validate to signup

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import * as UserController from '../controllers/user.controller'
+import validateSignUp from 'middlewares/validations/validate'
 
 const router = express.Router()
 
-router.post('/signup', UserController.signUp)
+router.post('/signup', validateSignUp, UserController.signUp)
 
 router.post('/login', UserController.login)
 
